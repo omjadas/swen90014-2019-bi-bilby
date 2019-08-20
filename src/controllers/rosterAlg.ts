5ebf422fd583cdbd995678fff2c560630908ffe6
João Pereira <joaopereira>
2019-08-20 10:46:23 +1000

    Update rosterAlg.ts

--- src/controllers/rosterAlg.ts
+++ src/controllers/rosterAlg.ts
@@ -1,45 +1,54 @@
-// import { generateTimesForDay } from "./generateTimes";
-// import { readData } from "./readData";
-// import { generateTimesAlreadyBooked } from "./getTimesBooked";
+//  import { generateTimesForDay } from "./generateTimes";
+//  import { readData } from "./readData";
+//  import { generateTimesAlreadyBooked } from "./getTimesBooked";
 
-// /**
-//  * Base function for generating the availabilitites for the facilitator
-//  * to choose on their specified day
-//  */
-// async function findAvailableTimes(): Promise<void> {
-//   const data = readData();
+//  /**
+//   * Base function for generating the availabilitites for the facilitator
+//   * to choose on their specified day
+//   */
+//  async function findAvailableTimes(): Promise<void> {
+//    const data = readData();
 
-//   // Need to update to define with data model
-//   const currentBookings = data.currentBookings;
-//   const teacherPreferences = data.teacherPreference;
+//    // Need to update to define with data model
+//    const currentBookings = data.bookings;
+//    const teacherPreferences = data.teacherPreference;
+//    //const facilitators = data.facilitators;
+//    //const guestSpeakers = data.guestSpeakers;
 
-//   // Get date for the preference
-//   const preferredDate = teacherPreferences[0].date;
-//   const year = Number(preferredDate.substring(0,4));
-//   const month = Number(preferredDate.substring(5,7));
-//   const day = Number(preferredDate.substring(8,10));
+//    // Create new booking instances acording to teacher preferences.
+//    const bookings = [];
 
-//   const possibleTimes = await generateTimesForDay(year, month - 1, day);
+//    for (let k = 0; k < teacherPreferences.lenght; k++) {
+//      bookings.push(newBooking(teacherPreferences[k]));
+//    }
 
-//   const blockedTimes = await generateTimesAlreadyBooked(currentBookings, teacherPreferences[0].area);
+//    // Get date for the preference
+//    const preferredDate = teacherPreferences[0].date;
+//    const year = Number(preferredDate.substring(0,4));
+//    const month = Number(preferredDate.substring(5,7));
+//    const day = Number(preferredDate.substring(8,10));
 
-//   // Remove all blocked out times from candidate list
-//   for (let i = 0; i < blockedTimes.length; i++) {
-//     for (let j = 0; j < possibleTimes.length; j++) {
-//       if (blockedTimes[i].getTime() === possibleTimes[j].getTime()) {
-//         // Remove the next 1.5 hours after clash
-//         possibleTimes.splice(j, 3);
-//       }
-//     }
-//   }
+//    const possibleTimes = await generateTimesForDay(year, month - 1, day);
 
-//   // for (let i = 0; i < possibleTimes.length; i++) {
-//   //   console.log(possibleTimes[i]);
-//   // }
+//    const blockedTimes = await generateTimesAlreadyBooked(currentBookings, teacherPreferences[0].area);
 
-//   // From remaining times, manually select preference and add to unconfirmed bookings
+//    // Remove all blocked out times from candidate list
+//    for (let i = 0; i < blockedTimes.length; i++) {
+//      for (let j = 0; j < possibleTimes.length; j++) {
+//        if (blockedTimes[i].getTime() === possibleTimes[j].getTime()) {
+//          // Remove the next 1.5 hours after clash
+//          possibleTimes.splice(j, 3);
+//        }
+//      }
+//    }
 
-// }
+//    // for (let i = 0; i < possibleTimes.length; i++) {
+//    //   console.log(possibleTimes[i]);
+//    // }
+
+//    // From remaining times, manually select preference and add to unconfirmed bookings
+
+//  }
 
 // findAvailableTimes();
 // >>>>>>> b7976871f3871018239ed66bb8ec78f614312001
