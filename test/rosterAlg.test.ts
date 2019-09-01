6a85f0ef72a23e545d6c8988e5bef00762fcc79a
matt01671 <matt01671@gmail.com>
2019-09-01 12:15:43 +1000

    updated roster alg to reflect data model

--- test/rosterAlg.test.ts
+++ test/rosterAlg.test.ts
@@ -1,3 +1,4 @@
 import rosterByPreferences from "../src/controllers/rosterAlg";
 
+// We want to pass in all the PENDING bookings
 // rosterByPreferences(teacherPreferences, guestSpeakers:, facilitators, locations)
