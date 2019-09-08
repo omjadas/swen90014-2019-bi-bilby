f3ea024874d772ad3d999549e2caa80bec81602d
muzamil <jimi252@yahoo.com>
2019-09-08 12:41:57 +1000

    writing into a json file

--- src/controllers/xlsheetcontroller.ts
+++ src/controllers/xlsheetcontroller.ts
@@ -1,12 +1,26 @@
 import * as XLSX from 'xlsx';
 import fs from 'fs';
+//import { json } from 'body-parser';
 
 const buf = fs.readFileSync("src/ExcelSheetIO/BigIssueRostering.xlsx");
 const wb = XLSX.read(buf, {type:'buffer'});
-wb.SheetNames.forEach(function(sheetName) {
-  const sheets = XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
-  const jsonobject = JSON.stringify(sheets);
-  console.log(jsonobject);
+
+// writing into a json file data.json
+fs.writeFile("src/ExcelSheetIO/data.json",JSON.stringify(wb,null,4),function(err){
+  if(err){
+    console.log(err);
+  }
+  console.log("Done");
 });
 
-XLSX.writeFile(wb,"src/ExcelSheetIO/ROsterSheet.xlsx");
+// writing into a excel sheet RosterSheet.xlsx
+XLSX.writeFile(wb,"src/ExcelSheetIO/RosterSheet.xlsx");
+
+
+// Code to get different sheets
+
+// wb.SheetNames.forEach(function(sheetName) {
+//   const sheets = XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
+//   const jsonobject = JSON.stringify(sheets,null,4);
+//   console.log(jsonobject);
+// });
