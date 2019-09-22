d0cabc6231f68efbb84a544e267d1d9f0ee0b3ef
Omja Das <odas@student.unimelb.edu.au>
2019-09-22 11:55:13 +1000

    send 400 error if file is not correct when uploaded

--- src/controllers/apiControllers.ts
+++ src/controllers/apiControllers.ts
@@ -11,18 +11,22 @@ import { Booking } from "../models/booking.model";
  * @param {Response} res response object
  * @returns {void}
  */
-export function upload (req: Request, res: Response): void {
-  if (req.files && !Array.isArray(req.files.excel)) {
-    const file = req.files.excel.data;
-    const cities = getCities(file);
-    const bookings: Booking[] = [];
-    cities.forEach(city => {
-      bookings.concat(getBooking(file, city.city, new Date(), new Date()));
-    });
-    const guestSpeakers = getGuestSpeakers(file);
-    const facilitators = getFacilitators(file);
-    const roster = rosterByPreferences(bookings, guestSpeakers, facilitators);
-  } else {
-    res.send("File not found");
+export function upload(req: Request, res: Response): any {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
   }
+
+  if (Array.isArray(req.files.excel)) {
+    return res.status(400).send('Only one file is allowed to be uploaded.');
+  }
+
+  const file = req.files.excel.data;
+  const cities = getCities(file);
+  const bookings: Booking[] = [];
+  cities.forEach(city => {
+    bookings.concat(getBooking(file, city.city, new Date(), new Date()));
+  });
+  const guestSpeakers = getGuestSpeakers(file);
+  const facilitators = getFacilitators(file);
+  const roster = rosterByPreferences(bookings, guestSpeakers, facilitators);
 }
