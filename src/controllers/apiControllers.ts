f70fc05518b2597f7134bbdb224cfdb51ca4675c
Omja Das <odas@student.unimelb.edu.au>
2019-09-22 11:48:34 +1000

    generate roster from uploaded file

--- src/controllers/apiControllers.ts
+++ src/controllers/apiControllers.ts
@@ -1,1 +1,28 @@
-export default {};
+import { Request, Response } from "express";
+import rosterByPreferences from "./rosterAlg";
+import { getCities, getBooking, getGuestSpeakers, getFacilitators } from "./excelOperations";
+import { Booking } from "../models/booking.model";
+
+/**
+ * Function to handle uploading an excel file and generate a roster.
+ *
+ * @export
+ * @param {Request} req request object
+ * @param {Response} res response object
+ * @returns {void}
+ */
+export function upload (req: Request, res: Response): void {
+  if (req.files && !Array.isArray(req.files.excel)) {
+    const file = req.files.excel.data;
+    const cities = getCities(file);
+    const bookings: Booking[] = [];
+    cities.forEach(city => {
+      bookings.concat(getBooking(file, city.city, new Date(), new Date()));
+    });
+    const guestSpeakers = getGuestSpeakers(file);
+    const facilitators = getFacilitators(file);
+    const roster = rosterByPreferences(bookings, guestSpeakers, facilitators);
+  } else {
+    res.send("File not found");
+  }
+}
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import * as controller from "../controllers/apiControllers";
 
 const router = express.Router();
+
+router.post("/upload", controller.upload);
 
 export default router;
