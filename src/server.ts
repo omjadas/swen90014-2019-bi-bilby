eaed3f48a9ec2bc995e364fab14ee42d06a9e561
Omja Das <odas@student.unimelb.edu.au>
2019-09-22 11:51:35 +1000

    app.used fileUpload

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import fileUpload from "express-fileupload";
 import api from "./routes/api";
 import "./db";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(fileUpload());
 app.use("/api", api);
 app.use(express.static("client/build"));
 
