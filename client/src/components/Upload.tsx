4248d6b6c6d3b64ae8e0a2dce6ead2b324a4cb4d
Omja Das <odas@student.unimelb.edu.au>
2019-09-29 14:38:49 +1000

    updated the frontEnd styling

--- client/src/components/Upload.tsx
+++ client/src/components/Upload.tsx
@@ -1,7 +1,59 @@
 import React from "react";
-import DatePicker from "react-datepicker";
+import { Button, Container, Form, InputGroup } from "react-bootstrap";
+import bsCustomFileInput from "bs-custom-file-input";
+import "../stylesheets/Upload.css";
 
-class Upload extends React.Component {
+interface MyState {
+  style: React.CSSProperties,
+}
+
+class Upload extends React.Component<{}, MyState> {
+
+  /**
+   * Creates an instance of Upload.
+   * @param {*} props props
+   * @memberof Upload
+   */
+  public constructor(props: any) {
+    super(props);
+
+    this.state = {
+      style: {} as React.CSSProperties
+    };
+
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  /**
+   * Update the dimensions
+   *
+   * @returns {void} void
+   * @memberof Upload
+   */
+  private updateDimensions(): void {
+    const nav = document.getElementById("navBar");
+
+    if (nav !== null) {
+      this.setState({
+        style: {
+          width: `${window.innerWidth}px`,
+          height: `${window.innerHeight - nav.offsetHeight}px`
+        } as React.CSSProperties
+      });
+    }
+  }
+
+  /**
+   * Initialise bsCustomFileInput
+   *
+   * @returns {void} nothing
+   * @memberof Upload
+   */
+  public componentDidMount(): void {
+    bsCustomFileInput.init();
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions);
+  }
 
   /**
    * Render the upload form.
@@ -11,16 +63,36 @@ class Upload extends React.Component {
    */
   public render(): JSX.Element {
     return (
-      <form
-        id="uploadForm"
-        action="/api/upload"
-        method="post"
-        encType="multipart/form-data">
-        <input type="file" accept=".xlsx" name="excel" />
-        <input type="date" name="from" />
-        <input type="date" name="to" />
-        <input type="submit" value="upload" />
-      </form>
+      <div style={this.state.style}>
+        <Container className="Upload h-100 d-flex">
+          <Form
+            id="uploadForm"
+            className="align-self-center"
+            action="/api/upload"
+            method="post"
+            encType="multipart/form-data">
+            <Form.Group>
+              <InputGroup>
+                <div className="custom-file">
+                  <input className="custom-file-input" id="fileUpload" type="file" accept=".xlsx" name="excel" />
+                  <label className="custom-file-label" htmlFor="fileUpload">Choose file</label>
+                </div>
+              </InputGroup>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>From Date</Form.Label>
+              <Form.Control type="date" name="from" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>To Date</Form.Label>
+              <Form.Control type="date" name="to" />
+            </Form.Group>
+            <Button type="submit">
+              Roster
+            </Button>
+          </Form>
+        </Container>
+      </div>
     );
   }
 
