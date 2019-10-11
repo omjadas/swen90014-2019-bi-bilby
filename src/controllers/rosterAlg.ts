52c96be6b420ad270712c6ce05e28c1940d66f95
João Pereira <joaopereira>
2019-10-11 17:12:08 +1100

    Update rosterAlg.ts

--- src/controllers/rosterAlg.ts
+++ src/controllers/rosterAlg.ts
@@ -27,6 +27,10 @@ export default function rosterByPreferences(bookings: Booking[], guestSpeakers: 
     let backToBackFacilitator = false;
     let backToBackGuestSpeaker = false;
 
+    const timeBegin = bookings[i].sessionTime.timeBegin;
+    const timeEnd = bookings[i].sessionTime.timeEnd;
+    const workshop = bookings[i].workshop;
+
     if (i > 0) {
       backToBackFacilitator = checkBackToBackFacilitator(bookings[i - 1], bookings[i]);
       backToBackGuestSpeaker = checkBackToBackGuestSpeaker(bookings[i - 1], bookings[i]);
@@ -34,8 +38,8 @@ export default function rosterByPreferences(bookings: Booking[], guestSpeakers: 
 
     if (!backToBackFacilitator && !backToBackGuestSpeaker) { // Neither facilitator nor guest speaker can do back to back.
       // From the user pool we select facilitators and guest speakers and check their availability for a specific booking
-      availableFacilitators = facilitators.filter(user => userAvailable(user, bookings[i].sessionTime.timeBegin, bookings[i].sessionTime.timeEnd));
-      availableGuestSpeakers = guestSpeakers.filter(user => userAvailable(user, bookings[i].sessionTime.timeBegin, bookings[i].sessionTime.timeEnd));
+      availableFacilitators = facilitators.filter(user => userAvailable(user, timeBegin, timeEnd));
+      availableGuestSpeakers = guestSpeakers.filter(user => userAvailable(user, timeBegin, timeEnd));
 
       if (i > 0) {
         availableFacilitators = availableFacilitators.filter(user => (user !== bookings[i - 1].facilitator));
@@ -43,8 +47,8 @@ export default function rosterByPreferences(bookings: Booking[], guestSpeakers: 
       }
 
       // Crosscheck the workshop's constraints with user's attributes
-      availableFacilitators = availableFacilitators.filter(user => eligible(user, bookings[i].workshop));
-      availableGuestSpeakers = availableGuestSpeakers.filter(user => eligible(user, bookings[i].workshop));
+      availableFacilitators = availableFacilitators.filter(user => eligible(user, workshop));
+      availableGuestSpeakers = availableGuestSpeakers.filter(user => eligible(user, workshop));
 
       if (availableFacilitators.length === 0) {
         availableFacilitators.push(EMPTY_FACILITATOR);
@@ -55,7 +59,7 @@ export default function rosterByPreferences(bookings: Booking[], guestSpeakers: 
       // Pair facilitators and guest speakers to follow the constraints
       for (let f = 0; f < availableFacilitators.length; f++) {
         for (let g = 0; g < availableGuestSpeakers.length; g++) {
-          const pair = pairTeams(availableFacilitators[f], availableGuestSpeakers[g], bookings[i].workshop);
+          const pair = pairTeams(availableFacilitators[f], availableGuestSpeakers[g], workshop);
 
           if (pair !== null) {
             teams.push(pair);
@@ -63,40 +67,36 @@ export default function rosterByPreferences(bookings: Booking[], guestSpeakers: 
         }
       }
     } else if (!backToBackFacilitator && backToBackGuestSpeaker) { // Facilitator from previous booking can't do back to back but guest speaker can.
-      availableFacilitators = facilitators.filter(user => userAvailable(user, bookings[i].sessionTime.timeBegin, bookings[i].sessionTime.timeEnd));
+      availableFacilitators = facilitators.filter(user => userAvailable(user, timeBegin, timeEnd));
       availableFacilitators = availableFacilitators.filter(user => (user !== bookings[i - 1].facilitator));
-      availableFacilitators = availableFacilitators.filter(user => eligible(user, bookings[i].workshop));
+      availableFacilitators = availableFacilitators.filter(user => eligible(user, workshop));
 
       const guestSpeaker = guestSpeakers.filter(user => user === bookings[i - 1].guestSpeaker)[0];
 
       if (availableFacilitators.length === 0) {
         availableFacilitators.push(EMPTY_FACILITATOR);
-      } if (availableGuestSpeakers.length === 0) {
-        availableGuestSpeakers.push(EMPTY_GUESTSPEAKER);
       }
 
       for (let f = 0; f < availableFacilitators.length; f++) {
-        const pair = pairTeams(availableFacilitators[f], guestSpeaker, bookings[i].workshop);
+        const pair = pairTeams(availableFacilitators[f], guestSpeaker, workshop);
 
         if (pair !== null) {
           teams.push(pair);
         }
       }
     } else if (backToBackFacilitator && !backToBackGuestSpeaker) { // Facilitator from previous booking can do back to back but guest speaker can't.
-      availableGuestSpeakers = guestSpeakers.filter(user => userAvailable(user, bookings[i].sessionTime.timeBegin, bookings[i].sessionTime.timeEnd));
+      availableGuestSpeakers = guestSpeakers.filter(user => userAvailable(user, timeBegin, timeEnd));
       availableGuestSpeakers = availableGuestSpeakers.filter(user => (user !== bookings[i - 1].guestSpeaker));
-      availableGuestSpeakers = availableGuestSpeakers.filter(user => eligible(user, bookings[i].workshop));
+      availableGuestSpeakers = availableGuestSpeakers.filter(user => eligible(user, workshop));
 
       const facilitator = facilitators.filter(user => user === bookings[i - 1].facilitator)[0];
 
-      if (availableFacilitators.length === 0) {
-        availableFacilitators.push(EMPTY_FACILITATOR);
-      } if (availableGuestSpeakers.length === 0) {
+      if (availableGuestSpeakers.length === 0) {
         availableGuestSpeakers.push(EMPTY_GUESTSPEAKER);
       }
 
       for (let g = 0; g < availableGuestSpeakers.length; g++) {
-        const pair = pairTeams(facilitator, availableGuestSpeakers[g], bookings[i].workshop);
+        const pair = pairTeams(facilitator, availableGuestSpeakers[g], workshop);
 
         if (pair !== null) {
           teams.push(pair);
@@ -107,8 +107,11 @@ export default function rosterByPreferences(bookings: Booking[], guestSpeakers: 
       bookings[i].guestSpeaker = bookings[i - 1].guestSpeaker;
       bookings[i].state = BookingState.UNCONFIRMED;
 
-      adjustAvailabilities((facilitators.filter(user => user === bookings[i].facilitator)[0]), bookings[i].sessionTime.timeBegin, bookings[i].sessionTime.timeEnd);
-      adjustAvailabilities((guestSpeakers.filter(user => user === bookings[i].guestSpeaker)[0]), bookings[i].sessionTime.timeBegin, bookings[i].sessionTime.timeEnd);
+      const facilitator = facilitators.filter(user => user === bookings[i].facilitator)[0];
+      const guestSpeaker = guestSpeakers.filter(user => user === bookings[i].guestSpeaker)[0];
+
+      adjustAvailabilities(facilitator, timeBegin, timeEnd);
+      adjustAvailabilities(guestSpeaker, timeBegin, timeEnd);
     }
     // Filter the pairs of users as to remove all accuracies of empty users whenever possible.
     teams = filterTeams(teams);
@@ -119,8 +122,8 @@ export default function rosterByPreferences(bookings: Booking[], guestSpeakers: 
       bookings[i].guestSpeaker = teams[index][1];
       bookings[i].state = BookingState.UNCONFIRMED;
 
-      adjustAvailabilities(teams[index][0], bookings[i].sessionTime.timeBegin, bookings[i].sessionTime.timeEnd);
-      adjustAvailabilities(teams[index][1], bookings[i].sessionTime.timeBegin, bookings[i].sessionTime.timeEnd);
+      adjustAvailabilities(teams[index][0], timeBegin, timeEnd);
+      adjustAvailabilities(teams[index][1], timeBegin, timeEnd);
     }
   }
 
