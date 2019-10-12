3dddd18aa6477aac2a77bd479a3fb61f06d8305c
Omja Das <odas@student.unimelb.edu.au>
2019-10-12 12:02:30 +1100

    fixed month

--- test/rosterAlg7.test.ts
+++ test/rosterAlg7.test.ts
@@ -1,11 +1,8 @@
-import rosterByPreferences from "../src/controllers/rosterAlg";
-import { getCities, getBookings, getGuestSpeakers, getFacilitators, printBooking } from "../src/controllers/excelOperations";
-import { Booking } from "../src/models/booking.model";
 import { rosterFile } from "../src/controllers/apiControllers";
 import fs from "fs";
 
 const buf = fs.readFileSync("test/data/BigIssueRostering.xlsx");
 
 test("bookings on different days", () => {
-  expect(rosterFile(new Date(2019, 8, 5), new Date(2019, 8, 9), buf));
+  expect(rosterFile(new Date(2019, 7, 5), new Date(2019, 7, 9), buf));
 });
