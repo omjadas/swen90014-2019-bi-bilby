f51570ca6ed16a84be0c90f2ec5814c3923746a0
Omja Das <odas@student.unimelb.edu.au>
2019-10-12 13:47:54 +1100

    updated test 7

--- test/rosterAlg7.test.ts
+++ test/rosterAlg7.test.ts
@@ -4,5 +4,5 @@ import fs from "fs";
 const buf = fs.readFileSync("test/data/BigIssueRostering.xlsx");
 
 test("bookings on different days", () => {
-  expect(rosterFile(new Date(2019, 7, 5), new Date(2019, 7, 9), buf));
+  expect(() => rosterFile(new Date(2019, 7, 5), new Date(2019, 7, 9), buf)).not.toThrow();
 });
